65025720415b1c4b7430ff0f508ba53fdb532b23
high489 <vitalii.matskaniuk@gmail.com>
2024-12-15 21:31:17 -0300

    test commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
-//import mkcert from 'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
     tsconfigPaths(),
-    //mkcert(),
+    mkcert(),
   ],
-  base: "/capybara-cambio-robot/",
+  base: "/CapybaraCambioRobot/",
   css: {
     preprocessorOptions: {
       scss: {
